0213e1acd648522c7f207121814bdd142de704f9
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 13:15:28 -1000

    Update getCountryData.py

--- getCountryData.py
+++ getCountryData.py
@@ -4,12 +4,13 @@ import bs4 as bs
 import csv
 import re
 
-
 #create source folder if it doesnt exist yet
 if not os.path.exists('country_dfs'):
     os.makedirs('country_dfs')
     
-response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/af.html')
+countryCode = "as"
+    
+response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode +'.html')
 soup = bs.BeautifulSoup(response.text, 'lxml')
 mainBlock = soup.find("div", {"class": "main-block"})
 expandcollapse = mainBlock.find("ul", {"class": "expandcollapse"})
