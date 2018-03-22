a7c8bc3e76b4e313d740bbfbe63c3eb448b0812e
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 00:39:18 -1000

    Update getCountryIDs.py

--- getCountryIDs.py
+++ getCountryIDs.py
@@ -3,15 +3,22 @@ import requests
 import bs4 as bs
 import csv
 import re
+
+
+#create source folder if it doesnt exist yet
+if not os.path.exists('country_dfs'):
+    os.makedirs('country_dfs')
     
 response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/af.html')
 soup = bs.BeautifulSoup(response.text, 'lxml')
 countrySelection = soup.find('select')
-#print(countrySelection)
-for row in countrySelection.findAll('option'):
-    valueStr = row.get('value')
-    start = "../geos/"
-    end = ".html"
-    countryCode = valueStr[len(start):-len(end)]
-    print(countryCode)
-    print(row.text)
+with open('country_dfs/countries.csv', 'a') as csvfile:
+    fieldnames = ['Symbol', 'Country']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator = '\n')
+    writer.writeheader()
+    for row in countrySelection.findAll('option'):
+        valueStr = row.get('value')
+        start = "../geos/"
+        end = ".html"
+        countryCode = valueStr[len(start):-len(end)]
+        writer.writerow({'Symbol': countryCode,'Country': row.text})
