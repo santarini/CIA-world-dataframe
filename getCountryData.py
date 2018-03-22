eeb4246d7642c4f34605a566ea4a3a3c7324bf80
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 12:32:29 -1000

    Update getCountryData.py

--- getCountryData.py
+++ getCountryData.py
@@ -13,13 +13,12 @@ response = requests.get('https://www.cia.gov/library/publications/the-world-fact
 soup = bs.BeautifulSoup(response.text, 'lxml')
 mainBlock = soup.find("div", {"class": "main-block"})
 expandcollapse = mainBlock.find("ul", {"class": "expandcollapse"})
-introductionSection = expandcollapse.findAll('li')[1]
+#introductionSection = expandcollapse.findAll('li')[1]
 geographySection = expandcollapse.findAll('li')[3]
 peopleAndSocietySection = expandcollapse.findAll('li')[5]
-
-#find ul class 'expandcollapse'
-#sectiontitle
-#find all lis
-print(geographySection)
-
-#mainBlock = mainBlock.find('main-block')
+#population
+population = peopleAndSocietySection.findAll("div", {"class": "category_data"})[0]
+populationGrowthRate = peopleAndSocietySection.findAll("div", {"class": "category_data"})[4]
+birthRate = peopleAndSocietySection.findAll("div", {"class": "category_data"})[5]
+deathRate = peopleAndSocietySection.findAll("div", {"class": "category_data"})[6]
+print(deathRate)
