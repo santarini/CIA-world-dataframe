81a42dcff52d7a9f7ec6732ec3f08bcdb737c4a3
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-21 18:19:45 -1000

    Create getCountryIDs.py

---
+++ getCountryIDs.py
@@ -0,0 +1,13 @@
+import os
+import requests
+import bs4 as bs
+import csv
+import re
+    
+response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/af.html')
+soup = bs.BeautifulSoup(response.text, 'lxml')
+countrySelection = soup.find('select')
+for row in countrySelection.findAll('option'):
+    countryCode = re.search('<option value="../geos/(.*).html', str(row))
+    print(countryCode)
+    print(row.text)
