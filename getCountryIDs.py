8ed202d30a29dda833157a17ab8f60e752d48c26
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-21 18:26:56 -1000

    Update getCountryIDs.py

--- getCountryIDs.py
+++ getCountryIDs.py
@@ -8,6 +8,6 @@ response = requests.get('https://www.cia.gov/library/publications/the-world-fact
 soup = bs.BeautifulSoup(response.text, 'lxml')
 countrySelection = soup.find('select')
 for row in countrySelection.findAll('option'):
-    countryCode = re.search('<option value="../geos/(.*).html', str(row))
+    countryCode = re.search('<option value="../geos/(.*).html">', str(row))
     print(countryCode)
     print(row.text)
