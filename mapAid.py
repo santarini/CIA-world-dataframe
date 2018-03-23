0653dfcd5d065759942871c4a32633c87398a113
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 17:34:19 -1000

    Create mapAid.py

---
+++ mapAid.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import bs4 as bs
+import csv
+import re
+
+with open("cleanCountries.csv") as csvfileA:
+      reader = csv.DictReader(csvfileA)
+      for row in reader:
+                countryName = (row['Country'])
+                countryCode = (row['Code'])
+                response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode +'.html')
+                soup = bs.BeautifulSoup(response.text, 'lxml')
+                
+            #population growth rate
+            populationGrowth = soup.body.find(text='Population growth rate:').findNext('div')
+            populationGrowth = populationGrowth.text
+            if "(" in populationGrowth:
+                popGrowthDateEst = populationGrowth.split('(')[1]
+                populationGrowth = populationGrowth.split('(')[0]
