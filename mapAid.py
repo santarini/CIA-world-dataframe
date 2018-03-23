f707a3030164d4af404701ee1849c96d0526f4fa
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 17:49:01 -1000

    Update mapAid.py

--- mapAid.py
+++ mapAid.py
@@ -4,19 +4,24 @@ import bs4 as bs
 import csv
 import re
 
+
 with open("cleanCountries.csv") as csvfileA:
     reader = csv.DictReader(csvfileA)
     for row in reader:
         countryName = (row['Country'])
         countryCode = (row['Code'])
-        response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode +'.html')
+        response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode + '.html')
         soup = bs.BeautifulSoup(response.text, 'lxml')
                 
         #population growth rate
-        populationGrowth = soup.body.find(text='Population growth rate:').findNext('div')
-        populationGrowth = populationGrowth.text
+        if "Population growth rate:" not in soup.text:
+            populationGrowth = 'None'
+        else:
+            populationGrowth = soup.body.find(text='Population growth rate:').findNext('div')
+            populationGrowth = populationGrowth.text
         if "(" in populationGrowth:
             popGrowthDateEst = populationGrowth.split('(')[1]
             populationGrowth = populationGrowth.split('(')[0]
+
 
         print(countryName + " " + populationGrowth)
