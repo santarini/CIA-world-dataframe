b8841ebb4e16075e8648a2b44a0f7ad545221526
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 16:27:25 -1000

    Update getPopData.py

--- getPopData.py
+++ getPopData.py
@@ -4,15 +4,16 @@ import bs4 as bs
 import csv
 import re
 
-with open("countryLI.csv") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        countryCode = (row['Code'])
-        countryName = (row['Country'])
-        response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode +'.html')
-        soup = bs.BeautifulSoup(response.text, 'lxml')
-        population = soup.body.find(text='Population:').findNext('div')
-        with open('countryPop.csv', 'a') as csvfile:
-            fieldnames = ['Country', 'Code','Population']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator = '\n')
+with open("cleanCountries.csv") as csvfileA:
+    reader = csv.DictReader(csvfileA)
+    with open('countryPop.csv', 'a') as csvfileB:
+        fieldnames = ['Country', 'Code','Population']
+        writer = csv.DictWriter(csvfileB, fieldnames=fieldnames, lineterminator = '\n')
+        writer.writeheader()
+        for row in reader:
+            countryName = (row['Country'])
+            countryCode = (row['Code'])
+            response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode +'.html')
+            soup = bs.BeautifulSoup(response.text, 'lxml')
+            population = soup.body.find(text='Population:').findNext('div')
             writer.writerow({'Country': countryName , 'Code': countryCode,'Population': population.text})
