e20563aa1c2a30fc81af9f1b9e09e88319c49176
Makoa <32736507+santarini@users.noreply.github.com>
2018-03-22 17:32:26 -1000

    Update getPopData.py

--- getPopData.py
+++ getPopData.py
@@ -7,7 +7,7 @@ import re
 with open("cleanCountries.csv") as csvfileA:
     reader = csv.DictReader(csvfileA)
     with open('countryPop.csv', 'a') as csvfileB:
-        fieldnames = ['Country', 'Code','Population']
+        fieldnames = ['Country', 'Code','Population', 'Population Growth Rate']
         writer = csv.DictWriter(csvfileB, fieldnames=fieldnames, lineterminator = '\n')
         writer.writeheader()
         for row in reader:
@@ -15,10 +15,12 @@ with open("cleanCountries.csv") as csvfileA:
             countryCode = (row['Code'])
             response = requests.get('https://www.cia.gov/library/publications/the-world-factbook/geos/' + countryCode +'.html')
             soup = bs.BeautifulSoup(response.text, 'lxml')
+
+            #population data
             population = soup.body.find(text='Population:').findNext('div')
             population = population.text
             if "(" in population:
-                dateEst = population.split('(')[1]
+                popDateEst = population.split('(')[1]
                 population = population.split('(')[0]
             if "total:" in population:
                 population = population.split('total:')[1]
@@ -27,4 +29,14 @@ with open("cleanCountries.csv") as csvfileA:
             if "million" in population:
                 population = population.split(' million')[0]
                 population = (float(population) * 1000000)
-            writer.writerow({'Country': countryName , 'Code': countryCode,'Population': population})
+
+            #population growth rate
+            populationGrowth = soup.body.find(text='Population growth rate:').findNext('div')
+            populationGrowth = populationGrowth.text
+            if "(" in populationGrowth:
+                popGrowthDateEst = populationGrowth.split('(')[1]
+                populationGrowth = populationGrowth.split('(')[0]
+            
+            #print to csv
+            writer.writerow({'Country': countryName , 'Code': countryCode,'Population': population, 'Population Growth Rate': populationGrowth})
+
